e514e9b76dbf18c3909fd208718a2b4be12dd761
Yehor Avramenko <yehor.avramenko@ukr.net>
2026-02-18 02:18:28 +0200

    Add Premake "clean" command

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,13 @@
 local td = "tramdepot"
 local clDir = td.."/client/"
 
+if _ACTION == "clean" then
+   os.rmdir("build")
+   os.rmdir(".vs")
+   os.remove(os.matchfiles("**.sln*"))
+   os.remove(os.matchfiles("**.vcxproj*"))
+end
+
 workspace "TramDepot"
    characterset "Unicode"
    architecture "x86_64"
@@ -10,3 +17,4 @@ workspace "TramDepot"
 
 group "Client"
     include "tramdepot/client/tramdepot.lua"
+        

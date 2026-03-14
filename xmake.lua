0341778efc41f42e4297d01c88a6175b4660fe1f
Yehor Avramenko <yehor.avramenko@ukr.net>
2026-03-14 23:41:01 +0100

    Delete xmake.lua

--- xmake.lua
+++
@@ -1,7 +0,0 @@
-add_rules("mode.debug", "mode.release", "plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
-
-target("tramdepot")
-    set_kind("binary")
-    add_files("tramdepot/**.cpp")
-    add_includedirs("$(scriptdir)/tramdepot/client")
-    add_links("glfw")
